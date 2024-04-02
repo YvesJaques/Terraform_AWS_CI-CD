148d0144351305ba94fa9dad5ce4303e1774d298
Yves Jaques <ymoraisjaques@gmail.com>
2024-04-01 21:11:35 -0300

    Fix tf fmt

--- src/main.tf
+++ src/main.tf
@@ -1,30 +1,30 @@
 terraform {
-    backend "s3" {
-        bucket = "cc-tf-state-backend-ci-cd-yves"
-        key = "tf-infra/terraform.tfstate"
-        region = "sa-east-1"
-        dynamodb_table = "terraform-state-locking"
-        encrypt = true
-    }
+  backend "s3" {
+    bucket         = "cc-tf-state-backend-ci-cd-yves"
+    key            = "tf-infra/terraform.tfstate"
+    region         = "sa-east-1"
+    dynamodb_table = "terraform-state-locking"
+    encrypt        = true
+  }
 
-    required_version = ">= 0.13.0"
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~>3.0"
-        }
+  required_version = ">= 0.13.0"
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~>3.0"
     }
+  }
 }
 
 provider "aws" {
-    region = "sa-east-1"
+  region = "sa-east-1"
 }
 
 module "tf-state" {
-    source = "./modules/tf-state"
-    bucket_name = "cc-tf-state-backend-ci-cd-yves"
+  source      = "./modules/tf-state"
+  bucket_name = "cc-tf-state-backend-ci-cd-yves"
 }
 
 module "dynamodb-infra" {
-    source = "./modules/dynamodb"
+  source = "./modules/dynamodb"
 }
